85c559887fc4ee2f063741e1e58e3ee3df722f56
AnEmortalKid <janmonterrubio@gmail.com>
2021-11-01 23:31:47 -0500

    counter display

--- src/renderer.js
+++ src/renderer.js
@@ -5,14 +5,15 @@ const { ipcRenderer } = require('electron');
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
 
-var sd = new StatusDisplayer();
-sd.onStart();
-sd.onStop();
+const sd = new StatusDisplayer();
 
 ipcRenderer.on(OBSEvents.RecordingStarted, () => {
   console.log('renderer.RecordingStarted');
+
+  sd.onStart();
 });
 
 ipcRenderer.on(OBSEvents.RecordingStopped, () => {
   console.log('renderer.RecordingStopped');
+  sd.onStop();
 });
--- src/status/statusDisplayer.js
+++ src/status/statusDisplayer.js
@@ -1,9 +1,54 @@
+const secondsInHour = 60 * 60;
+const secondsInMinute = 60;
+
+function toDurationString(seconds) {
+  if (seconds < 0) {
+    return "";
+  }
+
+  var remaining = seconds;
+
+  var hours = Math.floor(remaining / secondsInHour);
+  remaining -= hours * secondsInHour;
+
+  var minutes = Math.floor(remaining / secondsInMinute);
+  remaining -= minutes * secondsInMinute;
+
+  var hoursStr = String(hours).padStart(2, 0);
+  var minStr = String(minutes).padStart(2, 0);
+  var secStr = String(remaining).padStart(2, 0);
+  
+  return hoursStr + ":" + minStr + ":" + secStr;
+}
+
 export default class StatusDisplayer {
+
+  constructor() {
+    this.timerSeconds = 0;
+    this.timerIntervalId = null;
+  }
+
+  timerTick() {
+    this.timerSeconds = this.timerSeconds +1;
+    this.updateLabel();
+  }
+
+  updateLabel() {
+    document.getElementById("statusText").textContent = toDurationString(this.timerSeconds);
+  }
 
   onStart() {
     console.log('OnStart');
+    this.timerSeconds = 0;
+
+    this.timerIntervalId = setInterval(this.timerTick.bind(this), 1000);
   }
+
   onStop() {
     console.log('OnStop');
+    clearInterval(this.timerIntervalId);
+    this.timerSeconds = 0;
+    this.updateLabel();
   }
+
 }
